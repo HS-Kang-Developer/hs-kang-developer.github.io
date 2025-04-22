5276df4014f7f400489efd5ce7e7be793cc865a2
Heesoo Kang <kang.lepra@gmail.com>
2025-04-22 18:35:05 +0900

    gmail追加

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 
+
 function Footer() {
   return (
     <footer className="footer">
@@ -8,6 +9,8 @@ function Footer() {
         <span className="logo">HEESOO<span className="accent">KANG</span></span>
       </div>
       <div className="footer-right">
+      <img src="/assets/icons/gmail.svg" className="gmail-icon" alt="Gmail Icon" />
+        <a href="mailto:audentia16@gmail.com" className="footer-email">audentia16@gmail.com</a>
         {/* <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
         <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
         <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
